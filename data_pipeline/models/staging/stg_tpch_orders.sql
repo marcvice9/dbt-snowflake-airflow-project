19a04ae9906ef0ea6e78eefcda599499b263e11b
marcvice9 <marcvicente9@gmail.com>
2025-02-06 17:31:46 +0100

    added sources and staging tables

---
+++ data_pipeline/models/staging/stg_tpch_orders.sql
@@ -0,0 +1,8 @@
+select
+    o_orderkey AS order_key,
+    o_custkey AS customer_key,
+    o_orderstatus AS order_status,
+    o_totalprice AS total_price,
+    o_orderdate AS order_date
+from
+    {{ source('tpch', 'orders') }}
