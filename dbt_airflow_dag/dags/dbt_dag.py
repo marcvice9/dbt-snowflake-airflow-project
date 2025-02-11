b418d6a701657f0259c6f5cb451ebe8bad700b91
marcvice9 <marcvicente9@gmail.com>
2025-02-11 11:37:37 +0100

    dbt_dag file

--- dbt_airflow_dag/dags/dbt_dag.py
+++ dbt_airflow_dag/dags/dbt_dag.py
@@ -14,7 +14,7 @@ profile_config = ProfileConfig(
 )
 
 dbt_snowflake_dag = DbtDag(
-    project_config=ProjectConfig("/usr/Desktop/git_projects/dbt-snowflake-airflow-project/data_pipeline",),
+    project_config=ProjectConfig("/usr/local/airflow/dags/data_pipeline",),
     operator_args={"install_deps": True},
     profile_config=profile_config,
     execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",),
